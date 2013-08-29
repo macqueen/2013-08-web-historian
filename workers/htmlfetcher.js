41fd322a67bbdbc36ee27226a49e11208cf6b3ae
Hack Reactor Students <github@hackreactor.com>
2013-08-28 17:48:19 -0700

    wrote script to pull down website content

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,5 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+var helpers = require('./lib/html-fetcher-helpers');
+
+helpers.readUrls(helpers.downloadUrls);
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,33 @@
-exports.readUrls = function(filePath, cb){
+var getter = require('http-get');
+var dir = require('../../web/request-handler');
+var fs = require('fs');
 
+exports.readUrls = function(cb){
+  fs.readFile(dir.datadir, 'utf8', function(error, content){
+    var urls = content.split('\n');
+    cb(urls);
+  });
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  for(var i = 0; i < urls.length; i++){
+    getter.get(urls[i], './data/sites/' + urls[i], function(error, result){
+      if(error){
+        console.log(error);
+      } else {
+        console.log('File downloaded: ' + result.file);
+      }
+    });
+  }
 };
+
+
+
+// var http = require('http-get');
+// http.get('http://localhost/foo.pdf', '/path/to/foo.pdf', function (error, result) {
+//  if (error) {
+//    console.error(error);
+//  } else {
+//    console.log('File downloaded at: ' + result.file);
+//  }
+// });
